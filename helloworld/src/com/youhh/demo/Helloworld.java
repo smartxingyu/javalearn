d05bd08c23d76b49ec2cd88ad5ab370db38b60cb
smartxingyu <839239794@qq.com>
2017-01-24 20:48:18 +0800

    first

---
+++ helloworld/src/com/youhh/demo/Helloworld.java
@@ -0,0 +1,13 @@
+package com.youhh.demo;
+
+/**
+ * Created by xingyu on 2017/1/24.
+ */
+import java.util.*;
+
+public class Helloworld {
+    public static void main(String[] args){
+        System.out.println("hello world");
+        System.out.println(new Date());
+    }
+}
